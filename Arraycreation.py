1614572affde2f56118a9dab73ae4e5b835a1cfc
Himanshubendale21 <himanshubendale88@gmail.com>
2026-01-24 00:05:17 +0530

    Add files via upload

---
+++ Arraycreation.py
@@ -0,0 +1,75 @@
+import numpy as np 
+
+# zero filled array
+
+# a = np.zeros(5)
+# print(a)
+
+# a = np.zeros((2,5))
+# print(a)
+
+# ones filled array
+
+# a = np.ones(10)
+# print(a)
+
+# a = np.ones((3 , 5))
+# print(a)
+
+# full array
+
+# s = np.full((4,5),2)
+# print(s)
+
+# arange array
+
+# h = np.arange(1, 10)
+# print(h)
+
+# h = np.arange(0 , 10 , 2)
+# print(h)
+
+# linspace array
+
+# d = np.linspace( 1, 10 , 5)
+# print(d)
+
+# identity array
+
+# a =  np.eye(3)
+# print(a)
+
+# random array
+
+# a = np.random.rand(5)
+# print(a)
+
+# a = np.random.rand(2 , 3)
+# print(a)
+
+# a= np.random.randint(1 , 20 ,5)
+# print(a)
+
+# empty array
+
+# a= np.empty (6)
+# print(a)
+
+# Create a 3×3 array of zeros
+
+# a = np.zeros((3,3))
+# print(a)
+
+# Create numbers from 10 to 50 with step 5
+
+# a = np.linspace(10,50,5)
+# print(a)
+
+# Create a 4×4 identity matrix
+# a = np.eye(4 , 4)
+# print(a)
+
+# Create 5 random integers between 1 and 100
+
+# a = np.random.randint(1 ,100)
+# print(a)
