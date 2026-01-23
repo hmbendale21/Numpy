639910338925d1d48ac7dd449e2629f867190ea7
Himanshubendale21 <himanshubendale88@gmail.com>
2026-01-23 21:28:28 +0530

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+# a = np.array([1,2,3,4])
+# print (a)
+
+# arr = np.array([1,2,3,4])
+# print (arr+1)
+
+# arr = np.array([1,2,3,4])
+# arr = arr + 2
+# print (arr)
+
+
+# arr = np.array([1,2,3,4,5])
+# print(arr.ndim)
+# print (arr.shape)
+# print (arr.dtype)
+# print (arr.itemsize)
+# print (arr.nbytes)
+
+
+# 2d array
+
+# matrix = np.array([
+#     [1,5,6],
+#     [8,4,2]
+# ])
+
+# print(matrix)
+# print (matrix.ndim)
+# print (matrix.shape)
+# print (matrix.dtype)
+# print (matrix.itemsize)
+
+
+# Indexing
+
+# 1D array
+
+# arr =np.array([10, 20 , 30 , 50 ])
+# print(arr[2])
+# print(arr[0:2])
+# print(arr[-1])
+
+
+# 2D array
+
+# matrix = np.array([
+#     [1,5,6],
+#     [8,4,2]
+# ])
+
+# print(matrix[0][2])
+# print(matrix[1][0])
+
+
+# Slicing
+
+# arr = np.array([1,2,5,8,6])
+# print (arr[0:4])
+# print(arr[:3])
+# print(arr[2:])
+
+
+# Q1 : Create a NumPy array of:[5, 10, 15, 20, 25]
+# Q2 :Add 5 to each element of the 
+# Q3 : Print the dimension , shape , data type , item size and number of bytes of the array
+
+# arr = np.array([5,10,15,20,25])
+# print (arr)
+
+# print(arr+5)
+
+# print (arr.ndim)
+# print (arr.shape)
+# print (arr.dtype)
+# print (arr.itemsize)
+# print (arr.nbytes)
