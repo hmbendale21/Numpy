6c81f6d0fd94a75909cbb4fc380ac4c5d1058421
Himanshubendale21 <himanshubendale88@gmail.com>
2026-05-27 23:43:43 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,38 @@
+# file = open ("Himanshu.txt", "w")
+# file.write("Hii Isshaaa")
+# file.close()
+
+# file = open ("Himanshu.txt", "r")
+# content = file.read()
+# print (content)
+# file.close()
+
+# file = open("Himanshu.txt", "a")
+# file.write("\nHow are you")
+# file.close()
+
+# file = open("Himanshu.txt", "r")
+# content = file.read()
+# print (content)
+# file.close()
+
+# with open ("Himanshu.txt" , "a") as file:
+#     file.write ("\nyou are soo cute")
+
+# with open ("Himanshu.txt" , "r") as file:
+#     content = file.read()
+#     print (content)
+
+
+ 
+# Username =input("Enter you username : ")
+
+# with open ("User.txt" , "a" ) as file :
+#     file.write (Username + "\n")
+
+#     print ("Username successfully saved")
+
+
+# with open ("User.txt", "r") as file:
+#     for user in file:
+#         print (user)
